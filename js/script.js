b0dc3ffa9049c798d778ba8133ae69bd75d669ae
Timur Kornilov <timadzr@gmail.com>
2021-06-27 18:15:35 +0300

    new js for site

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const g = new Dracula.Graph
+for (var j = 0; j < i; i++) {
+    n += i
+}
+g.addEdge('Tim', 'Timooon')
+const layouter = new Dracula.Layout.Spring(g)
+const renderer = new Dracula.Renderer.Raphael('#canvas', g, 1500, 600)
+renderer.draw()
+
+
